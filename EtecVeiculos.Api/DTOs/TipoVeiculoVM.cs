b5a3a6356c60430768c1d5277ca79f2257b5151c
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-06-17 20:38:14 -0300

    adicionado o controller de tipo Veiculos

---
+++ EtecVeiculos.Api/DTOs/TipoVeiculoVM.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+namespace EtecVeiculos.Api.DTOs;
+
+
+    public class TipoVeiculoVM
+    {
+    [Required]
+    [StringLength(30)]
+    [Display(Name = "Nome")]
+    public string Name { get; set; }
+
+    }
+
+
+
+
+ 
